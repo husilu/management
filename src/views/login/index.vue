4ccd20a23356cf58873ffc072423eece71f2d28b
husilu <714429363@qq.com>
2022-03-19 00:22:32 +0800

    chore: 添加全局css样式，添加request文件，添加登录页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class='login-wrap'>
+    <el-form :model="form" label-width="70px" class='login-form'>
+      <el-form-item label="用户名">
+        <el-col :span="22">
+          <el-input v-model="form.username" />
+        </el-col>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-col :span="22">
+          <el-input v-model="form.password" />
+        </el-col>
+      </el-form-item>
+       <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log('login')
+    }
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+.login-wrap {
+  height: 100vh;
+  width: 100vw;
+  background: url("../../assets/dva.jpeg") no-repeat;
+  background-size: 100% 100%;
+  display: flex;
+  overflow: hidden;
+  align-items: center;
+  justify-content: center;
+  .login-form {
+    padding-top: 20px;
+    width: 500px;
+    background: rgba($color: #fff, $alpha: 0.3)
+  }
+}
+</style>
