f323526ce78640b335329e37aea3916125be23e4
husilu <714429363@qq.com>
2022-03-19 00:22:49 +0800

    chore: 修改路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,31 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import layout from '@/components/layout/index.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/dashboard'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/',
+    name: 'Home',
+    component: layout,
+    children: [
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        meta: {
+            title: '系统首页'
+        },
+        component: () => import ( /* webpackChunkName: "dashboard" */ "@/views/dashboard/index.vue")
+      },
+    ]
+  },
+  {
+    path: '/login',
+    name: 'home',
+    component: () => import('../views/login/index.vue')
+  },
 ]
 
 const router = createRouter({
