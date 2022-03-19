b7d0238f25b837e0d08909dd1c78506a1e20eb75
husilu <15928476151@163.com>
2022-03-19 16:20:49 +0800

    chreo: 关闭需要根元素校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
-    'vue/no-parsing-error': 'off'
+    'vue/no-parsing-error': 'off',
+    'vue/valid-template-root': 'off'
   }
 }
